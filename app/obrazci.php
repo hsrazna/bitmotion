0789e6315fbf54e041dcf1e80cb3134087a58f48
iwebnal <iwebnal@gmail.com>
2017-07-05 14:38:19 +0300

    a

---
+++ app/obrazci.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+	<meta charset="utf-8">
+
+	<title>Title</title>
+	<meta name="description" content="">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<meta property="og:image" content="path/to/image.jpg">
+	<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+
+	
+	<link rel="stylesheet" href="libs/bootstrap-grid/bootstrap-grid.css">
+	<link rel="stylesheet" href="libs/font-awesome.min.css">
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css">
+	<!-- <link rel="stylesheet" href="libs/jscrollpane/jquery.jscrollpane.css"> -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.css" />
+	<link rel="stylesheet" href="css/main.min.css">
+
+
+	<!-- Chrome, Firefox OS and Opera -->
+	<meta name="theme-color" content="#000">
+	<!-- Windows Phone -->
+	<meta name="msapplication-navbutton-color" content="#000">
+	<!-- iOS Safari -->
+	<meta name="apple-mobile-web-app-status-bar-style" content="#000">
+
+</head>
+
+<body>
+
+	<div class="obrazecwrapp">
+		
+		<div class="header-itemwrapp">
+			<div class="header-item">
+				<div class="header-itemimg">
+					<img src="img/header-item.png" class="responsive-img" alt="img">
+				</div>
+				<div class="header-itemcontent">
+					<span class="header-itemcontent-span">Минимальное<br>вложение - от $50</span>
+				</div>
+			</div>
+			<div class="header-item">
+				<div class="header-itemimg">
+					<img src="img/header-item1.png" class="responsive-img" alt="img">
+				</div>
+				<div class="header-itemcontent">
+					<span class="header-itemcontent-span">Пассивный доход от 17,7% до 29,7% в месяц</span>
+				</div>
+			</div>
+			<div class="header-item">
+				<div class="header-itemimg">
+					<img src="img/header-item2.png" class="responsive-img" alt="img">
+				</div>
+				<div class="header-itemcontent">
+					<span class="header-itemcontent-span">Бесплатная<br>регистрация в системе</span>
+				</div>
+			</div>
+			<div class="header-item">
+				<div class="header-itemimg">
+					<img src="img/header-item3.png" class="responsive-img" alt="img">
+				</div>
+				<div class="header-itemcontent">
+					<span class="header-itemcontent-span">Официальная и прозрачная деятельность</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="header-linkbox">
+			<a href="#" class="header-link">
+				<span>Хочу инвестировать</span>
+			</a>
+		</div>
+
+
+
+
+
+
+
+
+
+
+
+	</div>
+	
+
+	<script src="libs/jquery/dist/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js"></script>
+	<!-- <script src="libs/jscrollpane/jquery.jscrollpane.min.js"></script> -->
+
+	<!-- <script src="libs/bootstrap/bootstrap.min.js"></script> -->
+	<script src="libs/owl.carousel/owl.carousel.min.js"></script>
+	<script src="js/scripts.min.js"></script>
+
+</body>
+</html>
