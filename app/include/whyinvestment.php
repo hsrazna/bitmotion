8d922d641c5718c8b12bd8dee54eb473c537898c
iwebnal <iwebnal@gmail.com>
2017-07-05 18:12:06 +0300

    a

--- app/include/whyinvestment.php
+++ app/include/whyinvestment.php
@@ -1,10 +1,16 @@
 <section class="whyinvestment">
     <div class="container">
-		<span class="header-title video-title">Кто такие Cryp Trade Capital</span>
-		<p class="video-subtitle">и почему тут Вы найдете надежную платформу<br>для получения пассивного дохода</p>
-		<div class="video-viewdiv">
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/0rq42Xe9Mv4" frameborder="0" allowfullscreen></iframe>
-		</div>
+		<span class="header-title video-title">Как все выглядит изнутри</span>
+		<p class="video-subtitle">В этом видео - кусочек одного из уроков в системе Bitmotion Club.<br> Посмотрите, чтобы узнать больше об обучении </p>
     </div>
 </section>
 
+<section class="videobox">
+	<div class="container">
+		<div class="video-viewdiv1">
+			<div class="video-viewdiv">
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/0rq42Xe9Mv4" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</div>
+	</div>
+</section>
