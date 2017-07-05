18697f0dd53f7d513bf6b0708ab75a8dc101269d
iwebnal <iwebnal@gmail.com>
2017-07-06 00:32:24 +0300

    video mobile

--- app/include/whyinvestment.php
+++ app/include/whyinvestment.php
@@ -2,14 +2,38 @@
     <div class="container">
 		<span class="header-title video-title">Как все выглядит изнутри</span>
 		<p class="video-subtitle">В этом видео - кусочек одного из уроков в системе Bitmotion Club.<br> Посмотрите, чтобы узнать больше об обучении </p>
+		<div class="videobox1">
+			<div class="video-viewdiv">
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/0rq42Xe9Mv4" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<div class="whyinvestment-linkbox">
+				<div class="whyinvestment-linkbox-left">
+					<span class="whyinvestment-linkbox-text video-linkbox-text">Хотите начать обучение? Кликайте</span>
+				</div>
+				<div class="whyinvestment-link">
+					<a href="#" class="header-link video-link">
+						<span>Хочу инвестировать</span>
+					</a>
+				</div>
+			</div>
+		</div>
     </div>
 </section>
-
 <section class="videobox">
 	<div class="container">
 		<div class="video-viewdiv1">
 			<div class="video-viewdiv">
 				<iframe width="560" height="315" src="https://www.youtube.com/embed/0rq42Xe9Mv4" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<div class="whyinvestment-linkbox">
+				<div class="whyinvestment-linkbox-left">
+					<span class="whyinvestment-linkbox-text">Хотите начать обучение? Кликайте</span>
+				</div>
+				<div class="whyinvestment-link">
+					<a href="#" class="header-link video-link">
+						<span>Хочу инвестировать</span>
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
